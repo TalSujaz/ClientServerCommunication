578e30055a51e4144b536098bfd9a36afb35bca4
Tal Sujaz <sujaztal@gmail.com>
2021-10-15 15:52:16 +0300

    Add files via upload

---
+++ vfdhui/src/vfdhui/SocketServer.java
@@ -0,0 +1,88 @@
+package vfdhui;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+public class SocketServer extends Thread {
+public static final int PORT_NUMBER = 8081;
+protected Socket socket;
+private SocketServer(Socket socket) {
+this.socket = socket;
+System.out.println("New client connected from " +socket.getInetAddress().getHostAddress());
+start();
+}
+public String reverse(String request) {
+	int index=request.lastIndexOf(request),i=0;
+	char [] temp=request.toCharArray();
+	char t;
+	while(i!=index) {
+		t=temp[i];
+		temp[i]=temp[index];
+		temp[index]=t;
+	}
+	request=temp.toString();
+	System.out.println(request);
+	return request;
+}
+public void run() {
+InputStream in = null;
+OutputStream out = null;
+try {
+in = socket.getInputStream();
+out = socket.getOutputStream();
+BufferedReader br = new BufferedReader(new InputStreamReader(in));
+
+String request;
+while ((request = br.readLine()) != null) {
+//	request=reverse(request);
+	request=request.repeat(2);
+	request=request.toUpperCase();
+System.out.println("Message received:" +request);
+
+request += '\n';
+out.write(request.getBytes());
+}
+} catch (IOException ex) {
+System.out.println("Unable to get streams from client");
+}
+finally {
+try {
+in.close();
+out.close();
+socket.close();
+} 
+catch (IOException ex) {
+ex.printStackTrace();
+}
+}
+}
+
+public static void main(String[] args) {
+  System.out.println("SocketServer Example");
+ServerSocket server = null;
+try {
+server = new ServerSocket(PORT_NUMBER);
+while (true) {
+/**
+* create a new {@link SocketServer} object for
+
+each connection
+
+* this will allow multiple client connections
+*/
+new SocketServer(server.accept());
+}
+}catch (IOException ex) {
+System.out.println("Unable to start server.");
+} finally {
+try {
+if (server != null)
+server.close();
+} catch (IOException ex) {
+
+ex.printStackTrace();
+}
+}}}
---
+++ vfdhui/src/vfdhui/ff.java
@@ -0,0 +1,5 @@
+package vfdhui;
+
+public class ff {
+
+}
